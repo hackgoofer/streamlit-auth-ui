bcde7ddaf76815b0332220e36fc746fb12fbe673
Sasha <sashashengyt@gmail.com>
2023-12-30 21:27:27 -0800

    release note

--- streamlit_auth_ui/__init__.py
+++ streamlit_auth_ui/__init__.py
@@ -6,7 +6,7 @@ import streamlit.components.v1 as components
 # the component, and True when we're ready to package and distribute it.
 # (This is, of course, optional - there are innumerable ways to manage your
 # release process.)
-_RELEASE = os.getenv("DEBUG", "").lower() == False
+_RELEASE = os.getenv("RELEASE", "").lower() == True
 
 # Declare a Streamlit component. `declare_component` returns a function
 # that is used to create instances of the component. We're naming this
@@ -18,6 +18,7 @@ _RELEASE = os.getenv("DEBUG", "").lower() == False
 # *only thing* you need to do to create the binding between Streamlit and
 # your component frontend. Everything else we do in this file is simply a
 # best practice.
+print(f"RELEASE: {_RELEASE}")
 if not _RELEASE:
     _component_func = components.declare_component(
         # We give the component a simple, descriptive name
